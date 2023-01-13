58c1344594248638d290bfa8d8516ddfcf84486e
eonsinde <olasinde.eon@gmail.com>
2023-01-13 10:31:48 +0100

    added loading dots component

---
+++ src/components/LoadingDots/LoadingDots.js
@@ -0,0 +1,13 @@
+import './loadingdots.css'
+
+const LoadingDots = () => {
+  return (
+    <span className={'root'}>
+      <span className={'dot'} key={`dot_1`} />
+      <span className={'dot'} key={`dot_2`} />
+      <span className={'dot'} key={`dot_3`} />
+    </span>
+  )
+}
+
+export default LoadingDots
